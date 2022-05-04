b107c1e2deaf04f2e49f87a64a47955d859d69c0
ChrisCisneros <60458069+ChrisCisneros@users.noreply.github.com>
2022-05-04 13:23:14 -0700

    Added more search criteria and grouped buttons

--- projectUI.py
+++ projectUI.py
@@ -1,7 +1,8 @@
 #UI for project
 
-from PySide6.QtWidgets import(QPushButton, QWidget, QComboBox, QLineEdit, QGroupBox, QApplication, QLabel, QVBoxLayout, QPushButton)
+from PySide6.QtWidgets import(QPushButton, QStackedLayout, QHBoxLayout, QCheckBox, QWidget, QComboBox, QLineEdit, QGroupBox, QApplication, QLabel, QVBoxLayout, QPushButton)
 from PySide6.QtCore import Slot
+import sys
 
 class ProjectUI(QWidget):
     def __init__(self):
@@ -10,15 +11,43 @@ class ProjectUI(QWidget):
         genre_List = ['Comedy', 'Horror', 'Mystery', 'Historical', '-none-']
         genre = QComboBox()
         genre.addItems(genre_List)
-        genreLabel = QLabel("Please choose a genre")
+        genreLabel = QLabel("Please choose a genre:")
+
+        playingLabel = QLabel("Is Playing")
+        playingBox = QCheckBox()
+
+        popularLabel = QLabel("Most Popular")
+        popularBox = QCheckBox()
+
+        ratingsLabel = QLabel("Ratings")
+        ratingsBox = QCheckBox()
+        
+        hbox1 = QHBoxLayout()
+        hbox2 = QVBoxLayout()
+
+        hbox1.addWidget(playingLabel)
+        hbox1.addWidget(playingBox)
+        hbox1.addWidget(popularLabel)
+        hbox1.addWidget(popularBox)
+        hbox1.addWidget(ratingsLabel)
+        hbox1.addWidget(ratingsBox)
+
+        hbox2.addWidget(genreLabel)
+        hbox2.addWidget(genre)
+
+        gbox1 = QGroupBox()
+        gbox1.setLayout(hbox1)
+
+        gbox2 = QGroupBox()
+        gbox2.setLayout(hbox2)
 
         screen = QVBoxLayout()
-
-        screen.addWidget(genreLabel)
-        screen.addWidget(genre)
+        screen.addWidget(gbox1)
+        screen.addWidget(gbox2)
 
         self.setLayout(screen)
         self.setWindowTitle('Movie Search Tool')
+        self.setGeometry(100, 100, 500, 500)
 
 app = QApplication([])
 myScreen = ProjectUI()
