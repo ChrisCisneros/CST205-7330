0af670178b066962075403e1f27f600c010434ce
emran <emarsala@csumb.edu>
2022-05-04 14:36:18 -0700

    practice

---
+++ test2.py
@@ -0,0 +1,24 @@
+movie_info = [
+    {
+        "Title": "Movie1",
+        "Year-Releaseed": 2000,
+        "Genres": ["Genre1", "Genre2", "Genre3"],
+        "Actors": ["Actor1", "Actor2", "Actor3"],
+        "Movie-Poster-URL": "url"
+    },
+    {
+        "Title": "Movie2",
+        "Year-Releaseed": 2000,
+        "Genres": ["Genre1", "Genre2", "Genre3"],
+        "Actors": ["Actor1", "Actor2", "Actor3"],
+        "Movie-Poster-URL": "url"
+    },
+    {
+        "Title": "Movie3",
+        "Year-Releaseed": 2000,
+        "Genres": ["Genre1", "Genre2", "Genre3"],
+        "Actors": ["Actor1", "Actor2", "Actor3"],
+        "Movie-Poster-URL": "url"
+    }
+]
+
