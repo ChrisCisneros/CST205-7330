3667ed981d6afc9c65c136340068c71dae78ddc9
Emran Arsala <73989490+emranarsala@users.noreply.github.com>
2022-05-18 16:04:38 -0700

    Update projectUI.py

--- projectUI.py
+++ projectUI.py
@@ -7,6 +7,8 @@ import urllib.request
 from pprint import pprint
 from movie_api_data import movie_info
 import sys
+import requests
+from io import BytesIO
 
 movieDict = {}
 
@@ -15,15 +17,16 @@ class ProjectUI(QWidget):
         super().__init__()
 
         self.searchBoxLineEdit = QLineEdit("Enter Keywords to Search Here")
-        self.searchBoxLineEdit.selectAll()
+        #self.searchBoxLineEdit.selectAll()
         self.searchBoxLineEdit.returnPressed.connect(self.onSearchButton)
 
         self.searchPushButton = QPushButton("Search")
         self.searchPushButton.clicked.connect(self.onSearchButton)
 
-        genre_List = ['Comedy', 'Horror', 'Mystery', 'Historical', '-none-']
-        genre = QComboBox()
-        genre.addItems(genre_List)
+        genre_List = ['Fantasy','Action','Adventure','Comedy', 'Horror', 'Mystery', 'Historical', '-none-']
+        self.genre = QComboBox()
+        self.genre.addItems(genre_List)
+        self.genre.currentIndexChanged.connect(self.onSearchButton)
         genreLabel = QLabel("Please choose a genre:")
 
         playingLabel = QLabel("Is Playing")
@@ -46,7 +49,7 @@ class ProjectUI(QWidget):
         hbox1.addWidget(ratingsBox)
 
         hbox2.addWidget(genreLabel)
-        hbox2.addWidget(genre)
+        hbox2.addWidget(self.genre)
         hbox2.addWidget(self.searchBoxLineEdit)
         hbox2.addWidget(self.searchPushButton)
 
@@ -66,68 +69,92 @@ class ProjectUI(QWidget):
 
     @Slot()
     def onSearchButton(self):
-        keywords = self.searchBoxLineEdit.text().lower()
-        # print(keywords)
-
-        preprocess(movie_info)
-        # pprint(movieDict)
-
-        getCount(movieDict, keywords)
-
+        movie_dict = {}    
         max_list = []
         max_val = 0
-        for key, value in movieDict.items():
-            for counter, term in enumerate(value):
-                if counter == 0:
+
+      # def preprocess(movie_info):
+#     for i in movie_info:
+#         movieDict[i["imdbID"]] = []
+
+
+        def preprocess(movie_info):
+
+           
+
+            for i in movie_info:
+                movie_dict[i["imdbID"]] = []
+
+                for j in i['Title'].split():
+                     movie_dict[i['imdbID']].append(j.rstrip(',').lower())
+                
+                for j in i['Actors'].split():
+                    movie_dict[i['imdbID']].append(j.rstrip(',').lower())
+                
+                for j in i['Genre'].split():
+                    movie_dict[i['imdbID']].append(j.rstrip(',').lower())
+            
+                movie_dict[i['imdbID']].insert(1,i['Year'])
+                movie_dict[i['imdbID']].insert(0,0)
+         
+
+            return movie_dict
+
+        
+
+        
+        preprocess(movie_info)
+      
+
+        selection  = self.genre.currentText()
+
+
+        def get_count(dict,search):
+            for key,value in dict.items():
+                if selection in value:
+                    value[0] += 1
+                for count,term in enumerate(value):
+                    if count >= 1:
+                      
+                        if term in search:
+                            #                          
+                            value[0] += 1
+                             
+                           
+            return dict
+
+
+        get_count(movie_dict,self.searchBoxLineEdit.text())
+
+        for key,value in movie_dict.items():    
+            for count,term in enumerate(value):
+                if count == 0:
                     if term >= max_val:
-                        if term > max_val and len(max_list) > 0:
+                        if term > max_val and len(max_list) > 0:                 
                             del max_list[:len(max_list)]
                         max_val = term
-                        if max_val > 0:
-                            max_list.append((value[1], key))
+                        if(max_val > 0):
+                            max_list.append(key)
+                            
+
         
-        max_list.sort()
-        # pprint(max_list)
 
-        for i in max_list:
-            # pprint("=" * 25 + i[1] + "\n" + "=" * 25)
-            for k in movie_info:
-                if i[1] == k["imdbID"]:
-                    posterURL = k["Poster"]
-                    posterURL = "https:" + posterURL.split(":", 1)[1]
-                    pprint("Poster URL: " + posterURL)
-
-                    urllib.request.urlretrieve(posterURL, "poster.jpg")
-                    img = Image.open("poster.jpg")
-                    img.show()
-
-
-
-def preprocess(movie_info):
-    for i in movie_info:
-        movieDict[i["imdbID"]] = []
-
-        for k in i["Title"].split():
-            movieDict[i["imdbID"]].append(k.rstrip(",").lower())
-
-        for k in i["Actors"].split():
-            movieDict[i["imdbID"]].append(k.rstrip(",").lower())
         
-        movieDict[i['imdbID']].insert(0, 0)
-        movieDict[i["imdbID"]].insert(1, i["Title"][0].lower())
-    
-    return movieDict
 
-def getCount(currDict, keywords):
-    for key, value in currDict.items():
-        for counter, term in enumerate(value):
-            if counter > 1:
-                if term in keywords:
-                    value[0] += 1
+        for i in movie_info:
+            if(i['imdbID']) == max_list[0]:
+                posterURL = i["Images"][0]
+                urllib.request.urlretrieve(posterURL, "poster.jpg")
+                img = Image.open("poster.jpg")
+                img.show()
+                
+               
     
-    return currDict
+        
+
+    
 
 app = QApplication([])
 myScreen = ProjectUI()
 myScreen.show()
-sys.exit(app.exec())
+sys.exit(app.exec()
